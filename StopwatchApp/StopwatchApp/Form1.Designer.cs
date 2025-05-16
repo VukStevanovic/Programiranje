73cff29556336fe7c82b8b49076bb1fe7bcb8e4e
VukStevanovic <vukstevanovic552@gmail.com>
2025-05-16 13:24:26 +0200

    git add -A

---
+++ StopwatchApp/StopwatchApp/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace StopwatchApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnLap = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lstLaps = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 32F, System.Drawing.FontStyle.Bold);
+            this.lblTime.Location = new System.Drawing.Point(100, 40);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(400, 70);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "00:00:00.0";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(43, 150);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 40);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(183, 150);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 40);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Snimi";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(313, 150);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(100, 40);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Učitaj";
+            // 
+            // btnLap
+            // 
+            this.btnLap.Location = new System.Drawing.Point(436, 150);
+            this.btnLap.Name = "btnLap";
+            this.btnLap.Size = new System.Drawing.Size(100, 40);
+            this.btnLap.TabIndex = 4;
+            this.btnLap.Text = "Krug";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(561, 150);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 40);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            // 
+            // lstLaps
+            // 
+            this.lstLaps.FormattingEnabled = true;
+            this.lstLaps.Location = new System.Drawing.Point(100, 210);
+            this.lstLaps.Name = "lstLaps";
+            this.lstLaps.Size = new System.Drawing.Size(460, 95);
+            this.lstLaps.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(700, 350);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnLap);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lstLaps);
+            this.Name = "Form1";
+            this.Text = "Stopwatch";
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnLap;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ListBox lstLaps;
+
+        #endregion
+    }
+}
+
---
+++ StopwatchApp/StopwatchApp/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopwatchApp
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private TimeSpan elapsed;
+        private DateTime startTime;
+        private bool running;
+        private List<TimeSpan> laps;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 100; // 0.1 second
+            timer.Tick += Timer_Tick;
+
+            elapsed = TimeSpan.Zero;
+            running = false;
+            laps = new List<TimeSpan>();
+
+            lblTime.Text = "00:00:00.0";
+            btnStart.Text = "Start";
+            btnSave.Text = "Snimi";
+            btnLoad.Text = "Učitaj";
+            btnLap.Text = "Krug";
+
+            btnStart.Click += BtnStart_Click;
+            btnSave.Click += BtnSave_Click;
+            btnLoad.Click += BtnLoad_Click;
+            btnLap.Click += BtnLap_Click;
+            btnReset.Click += BtnReset_Click;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            elapsed = DateTime.Now - startTime;
+            lblTime.Text = elapsed.ToString(@"hh\:mm\:ss\.f");
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (!running)
+            {
+                startTime = DateTime.Now - elapsed;
+                timer.Start();
+                btnStart.Text = "Stop";
+                running = true;
+            }
+            else
+            {
+                timer.Stop();
+                elapsed = DateTime.Now - startTime;
+                btnStart.Text = "Start";
+                running = false;
+            }
+        }
+
+        private void BtnLap_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                laps.Add(elapsed);
+                lstLaps.Items.Insert(0, $"Lap {laps.Count}: {elapsed.ToString(@"hh\:mm\:ss\.f")}");
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Text Files (*.txt)|*.txt";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter sw = new StreamWriter(sfd.FileName))
+                    {
+                        sw.WriteLine(elapsed.ToString());
+                        foreach (var lap in laps)
+                        {
+                            sw.WriteLine(lap.ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Text Files (*.txt)|*.txt";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = File.ReadAllLines(ofd.FileName);
+                    if (lines.Length > 0 && TimeSpan.TryParse(lines[0], out TimeSpan loaded))
+                    {
+                        elapsed = loaded;
+                        lblTime.Text = elapsed.ToString(@"hh\:mm\:ss\.f");
+                        if (running)
+                        {
+                            startTime = DateTime.Now - elapsed;
+                        }
+                        laps.Clear();
+                        lstLaps.Items.Clear();
+                        for (int i = 1; i < lines.Length; i++)
+                        {
+                            if (TimeSpan.TryParse(lines[i], out TimeSpan lap))
+                            {
+                                laps.Add(lap);
+                                lstLaps.Items.Add($"Lap {i}: {lap.ToString(@"hh\:mm\:ss\.f")}");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid file format.");
+                    }
+                }
+            }
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            elapsed = TimeSpan.Zero;
+            lblTime.Text = "00:00:00.0";
+            btnStart.Text = "Start";
+            running = false;
+            laps.Clear();
+            lstLaps.Items.Clear();
+        }
+    }
+}
